1823944f02e7c95ef63e846d93028f08b98b14a2
cardosineo98 <lucasegbl@gmail.com>
2022-07-07 22:37:25 -0300

    Update script.js

--- array e objetos/29_calculadora/script.js
+++ array e objetos/29_calculadora/script.js
@@ -1,3 +1,19 @@
-let calculadora = {
-    "soma": function()
+const calculadora = {
+    soma: function(a,b) {
+        return a + b;
+    },
+    subtrair: function(a,b) {
+        return a - b;
+    },
+    produto: function(a,b) {
+        return a * b;
+    },
+    divisao: function(a,b) {
+        return a / b;
+    },
 }
+
+console.log(calculadora.soma(4,5))
+console.log(calculadora.subtrair(10,5));
+console.log(calculadora.produto(10,18));
+console.log(calculadora.divisao(25,5));
